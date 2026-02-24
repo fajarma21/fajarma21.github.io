5ac6c162b06eb02806d5744cfdbe861ae5394d73
Fajar Maulana <fajarma21@gmail.com>
2026-02-24 15:58:51 +0700

    fix(tooltip): fix update tooltip

--- src/components/Contacts/View.tsx
+++ src/components/Contacts/View.tsx
@@ -1,12 +1,16 @@
 import { FMContacts } from 'fajarma-react-lib';
 
+import css from './View.module.scss';
 import { LINKS } from './View.constants';
 import type { ContactsProps } from './View.types';
 
 const Contacts = ({ tooltipPosition = 'bottom' }: ContactsProps) => {
-  const tooltipOffset = tooltipPosition === 'top' ? { top: -20 } : { top: 50 };
-
-  return <FMContacts links={LINKS} tooltipOffset={tooltipOffset} />;
+  return (
+    <FMContacts
+      className={`${css.contact} ${css[tooltipPosition]}`}
+      links={LINKS}
+    />
+  );
 };
 
 export default Contacts;
