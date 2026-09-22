9e97c3e51a36848eb84e064308a11039c4c41d2f
Fajar Maulana <fajarma21@gmail.com>
2026-09-22 16:38:04 +0700

    feat(scroll): touch scroll page

--- src/hooks/usePageScroll/index.ts
+++ src/hooks/usePageScroll/index.ts
@@ -4,24 +4,12 @@ import type { UsePageScrollParams } from './index.types';
 
 const usePageScroll = ({ pageLength }: UsePageScrollParams) => {
   const [currentPage, setCurrentPage] = useState(0);
+  const touchstartY = useRef<number>(undefined);
   const isScrolling = useRef(false);
 
-  const scrollToSection = (index: number) => {
-    isScrolling.current = true;
-    setCurrentPage(index);
-    setTimeout(() => {
-      isScrolling.current = false;
-    }, 1000);
-  };
-
-  const handleWheel = useCallback(
-    (e: WheelEvent) => {
-      if (e.cancelable) e.preventDefault();
-
-      if (!(e.target instanceof HTMLElement)) return;
-      const scrollableChild = e.target.closest('[data-scrollable="true"]');
-      const deltaY = e.deltaY;
-
+  const scrollLogic = useCallback(
+    (target: HTMLElement, deltaY: number) => {
+      const scrollableChild = target.closest('[data-scrollable="true"]');
       if (scrollableChild) {
         const scrollTop = scrollableChild.scrollTop;
         const scrollHeight = scrollableChild.scrollHeight;
@@ -39,17 +27,55 @@ const usePageScroll = ({ pageLength }: UsePageScrollParams) => {
       if (deltaY > 0) newIndex++;
       else if (deltaY < 0) newIndex--;
 
-      scrollToSection(Math.min(Math.max(newIndex, 0), pageLength - 1));
+      isScrolling.current = true;
+      setCurrentPage(Math.min(Math.max(newIndex, 0), pageLength - 1));
+      setTimeout(() => {
+        isScrolling.current = false;
+      }, 1000);
     },
     [currentPage, pageLength],
   );
 
+  const handleWheel = useCallback(
+    (e: WheelEvent) => {
+      if (e.cancelable) e.preventDefault();
+      if (!(e.target instanceof HTMLElement)) return;
+      const deltaY = e.deltaY;
+
+      scrollLogic(e.target, deltaY);
+    },
+    [scrollLogic],
+  );
+
+  const handleTouchStart = useCallback((e: TouchEvent) => {
+    touchstartY.current = e.touches[0].clientY;
+  }, []);
+
+  const handleTouchMove = useCallback(
+    (e: TouchEvent) => {
+      if (e.cancelable) e.preventDefault();
+      if (!touchstartY.current || !(e.target instanceof HTMLElement)) return;
+      const deltaY = (e.touches[0].clientY - touchstartY.current) * -1;
+
+      scrollLogic(e.target, deltaY);
+    },
+    [scrollLogic],
+  );
+
   useEffect(() => {
     document.addEventListener('wheel', handleWheel, { passive: true });
+    document.addEventListener('touchstart', handleTouchStart, {
+      passive: true,
+    });
+    document.addEventListener('touchmove', handleTouchMove, {
+      passive: true,
+    });
     return () => {
       document.removeEventListener('wheel', handleWheel);
+      document.removeEventListener('touchstart', handleTouchStart);
+      document.removeEventListener('touchmove', handleTouchMove);
     };
-  }, [handleWheel]);
+  }, [handleTouchMove, handleTouchStart, handleWheel]);
 
   return { currentPage };
 };
